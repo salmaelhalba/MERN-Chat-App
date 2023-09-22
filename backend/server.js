0e002c01fd650fa57cbe6b35c380611cb2ddbfe5
salmaelhalba <salmael.2410@gmail.com>
2023-09-22 16:21:36 +0100

    add second api

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express()
 
@@ -8,7 +9,15 @@ app.get("/", (req, res) => {
 
 
 
-app.get()
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
 
 
 
